e04473dcba7a6ae3663402ff7621d1b600dfbcd1
Dinis Cruz <dinis.cruz@owasp.org>
2025-12-08 12:34:06 +0000

    added slides folder to lambda package creation

--- presentation_blackhat_eu_dec_2025/utils/deploy/Deploy__Service.py
+++ presentation_blackhat_eu_dec_2025/utils/deploy/Deploy__Service.py
@@ -1,11 +1,13 @@
-from osbot_fast_api_serverless.deploy.Deploy__Serverless__Fast_API  import Deploy__Serverless__Fast_API
-from presentation_blackhat_eu_dec_2025.config                                  import SERVICE_NAME, LAMBDA_DEPENDENCIES__BASE__SERVICE
-from presentation_blackhat_eu_dec_2025.fast_api.lambda_handler                 import run
+import presentation_blackhat_eu_dec_2025__slides
+from osbot_fast_api_serverless.deploy.Deploy__Serverless__Fast_API              import Deploy__Serverless__Fast_API
+from presentation_blackhat_eu_dec_2025.config                                   import SERVICE_NAME, LAMBDA_DEPENDENCIES__BASE__SERVICE
+from presentation_blackhat_eu_dec_2025.fast_api.lambda_handler                  import run
 
 class Deploy__Service(Deploy__Serverless__Fast_API):
 
     def deploy_lambda(self):
         with super().deploy_lambda() as _:
+            _.add_folder(presentation_blackhat_eu_dec_2025__slides.path)
             # Add any service-specific environment variables here
             # Example: _.set_env_variable('BASE_API_KEY', get_env('BASE_API_KEY'))
             return _
