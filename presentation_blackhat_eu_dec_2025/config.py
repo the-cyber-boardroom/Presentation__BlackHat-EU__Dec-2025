ff3eff9e069dd117af6698f600a92c19963e236b
Dinis Cruz <dinis.cruz@owasp.org>
2025-12-08 12:18:41 +0000

    fixed lambda version of osbot-fast-api-serverless

--- presentation_blackhat_eu_dec_2025/config.py
+++ presentation_blackhat_eu_dec_2025/config.py
@@ -3,7 +3,7 @@ from presentation_blackhat_eu_dec_2025 import package_name
 SERVICE_NAME                       = package_name
 FAST_API__TITLE                    = "Dinis Cruz = Presentation BlackHat EU Dec 2025"
 FAST_API__DESCRIPTION              = "Backend service for Dinis Cruz Presentation BlackHat EU Dec 2025"
-LAMBDA_DEPENDENCIES__BASE__SERVICE = ['osbot-fast-api-serverless==v1.2.0']
+LAMBDA_DEPENDENCIES__BASE__SERVICE = ['osbot-fast-api-serverless==v1.31.0']
 
 
 UI__SLIDES__ROUTE__SLIDES   = 'slides'
